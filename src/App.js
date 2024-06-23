09c29e51b0ea542a76fa5b620f9f3716880cbb72
Junaidve3 <junaid.nayeem@ve3.global>
2024-06-23 18:06:44 +0530

    vercel analytics uupdate

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.scss";
 import Main from "./containers/Main";
+import {Analytics} from "@vercel/analytics/react";
 
 function App() {
   return (
     <div>
+      <Analytics />
       <Main />
     </div>
   );
